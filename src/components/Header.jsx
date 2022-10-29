7502d60495b78b3d1edbe0b1495680f045f4afd7
juan pablo aranda <kingnero199412@gmail.com>
2022-10-28 20:44:32 -0500

    agrego funcionalidad al menu hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,40 @@
-import logo from "../assets/logo.png";
+import logo from '../assets/logo.png'
+import { useState } from 'react'
+
 
 const Header = () => {
-  let navbar = document.getElementById("navbar");
-
-  const toggle_nav = () => {
-    navbar.classList.toggle("hidden");
-  };
-
-  // Close menu is window size increases
-  window.onresize = () => {
-    let viewportWidth = window.innerWidth;
-    if (viewportWidth > 1050) {
-      navbar.classList.add("hidden");
-    }
-  };
-
+  let Links =[
+    {name:"Inicio",link:"/"},
+    {name:"Registro",link:"/"},
+    {name:"Iniciar Sesión",link:"/"},
+    
+    
+  ];
+  let [open,setOpen]=useState(false)
   return (
-    <div>
-      <nav class="bg-sky-900 flex text-teal-100 items-center justify-between flex-wrap pb-2 px-5">
-        <div className="flex justify-center items-center text-xl  font-semibold">
-          <img src={logo} alt="logo" className="mr-5 h-10 " />
-          <p>Centro de Convenciones</p>
+    <div className=' shadow-md w-full fixed top-0 left-0 bg-sky-800'>
+      <div className=' md:flex items-center justify-between py-4 md:px-10 px-7'>
+        <div className=' className="flex justify-center text-xl  font-semibold" cursor-pointer flex items-center'>
+          <span className=' mr-1 pt-2'>
+            <img src={logo} alt="logo" className=' w-20 h-20' />
+          </span>
+         <p className=''> Centro de Convenciones </p>
         </div>
-
-        <div class="block lg:hidden">
-          <button
-            onClick={toggle_nav}
-            class="flex flex-col px-3 py-2 border rounded border-gray-800 hover:text-white hover:border-white">
-            <div class="w-6 h-1 bg-white"></div>
-            <div class="w-6 h-1 bg-white my-1"></div>
-            <div class="w-6 h-1 bg-white"></div>
-          </button>
+        <div onClick={() =>setOpen(!open)} className=' text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+          <ion-icon name={open ? 'close': 'menu'}></ion-icon>
         </div>
-
-        <div
-          class="w-full lg:w-auto  lg:items-center text-justify lg:inline-block  hidden"
-          id="navbar">
-          <div class="lg:flex-grow text-2xl text-center space-x-3">
-            <a href="#" class="block lg:inline-block hover:underline mt-4">
-              Inicio
-            </a>
-            <a href="#" class="block lg:inline-block hover:underline mt-4">
-              Eventos
-            </a>
-            <a
-              href="/register"
-              class="block lg:inline-block hover:underline mt-4 mb-3z">
-              Registrarse
-            </a>
-            <a
-              href="/login"
-              class="block lg:inline-block hover:underline mt-4 mb-3z">
-              Iniciar Sesion
-            </a>
-          </div>
-        </div>
-      </nav>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-sky-800 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <a href={link.link} className='text-gray-800 hover:text-lime-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+      </ul>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header

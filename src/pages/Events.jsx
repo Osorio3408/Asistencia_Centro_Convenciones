662417121b900f74f663f2dea7f5a42628fa7821
juan pablo aranda <kingnero199412@gmail.com>
2022-11-06 13:08:07 -0500

    pagina de eventos finalizada

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,16 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ImageCard from '../components/ImageCard';
+import ImageSearch from '../components/ImageSearch';
+import Header from '../components/Header'
 import Footer from "../components/Footer";
-import Header from "../components/Header";
-import BodyHome from "../components/BodyHome";
 
 
-const Events = () => {
+function Events() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=31134368-cbf7aafd582ab6e1e41ca089a&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
-    <div>
-      <Header />
-         
-      <Footer />
+    <>
+      <Header/>
+    <div className="container mx-auto">
+        <ImageSearch searchText={(text) => setTerm(text)} />
+        {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+        {isLoading ? 
+          <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :
+          <div className="grid grid-cols-3 gap-4">
+            {images.map(image => <ImageCard key={image.id} image={image} />)}
+          </div>
+        }
     </div>
+      <Footer/>
+    </>
   );
-};
+}
 
 export default Events;
